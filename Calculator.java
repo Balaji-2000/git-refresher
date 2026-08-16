c7612bf7d6e60deab1c16a11d953620ce3aff38e
Balaji-2000 <110401732+Balaji-2000@users.noreply.github.com>
2026-08-16 20:47:33 +0530

    Created Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+
+public class Calculator {
+	
+	   public int add(int a, int b) {
+		   return a+b;
+	   }
+	   public int subtract(int a, int b) {
+		   return a-b;
+	   }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calc = new Calculator();
+		System.out.println("Sum: " + calc.add(5, 3));
+		System.out.println("Diff: " + calc.subtract(5, 3));
+
+	}
+
+}
