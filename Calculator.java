b44857de90c8d33187992142bac8860c10b87bd0
balaji-2000 <balajispty132@gmail.com>
2026-08-18 22:25:06 +0530

    making to merge conflict

--- Calculator.java
+++ Calculator.java
@@ -32,7 +32,7 @@ public class Calculator {
 		System.out.println("Modulus: " + calc.modulus(5, 2));
         System.out.println("Power: " + calc.power(5, 2));		
 		
-		System.out.println("addind to check git pull");		
+		System.out.println("addind to check stash command");		
 		
 
 	}
